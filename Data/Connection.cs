9793c173c59bf950488356c5dd363c4d6c7b1d18
oddonenso@gmail.com <oddonenso@gmail.com>
2025-03-09 19:31:04 +0700

    Added migration in Project

---
+++ Data/Connection.cs
@@ -0,0 +1,23 @@
+﻿using Data.Tables;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public class Connection :DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<ToyImage> ToyImages { get; set; }
+        public DbSet<Toy> Toys { get; set; }
+
+        public Connection(DbContextOptions<Connection> options) :base(options)
+        {
+
+        }
+    }
+}
